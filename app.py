608103374d9ad69c453eb1bbe016dc1d641f7286
ysugiyama12 <yuji9511yuji@gmail.com>
2019-12-16 16:35:07 +0900

    add elasticsearch

--- app.py
+++ app.py
@@ -7,14 +7,16 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def index():
     params = {}
-    keys = ["keyword", "user_id", "language"]
+    keys = ["keyword", "user_id", "language", "result"]
+    for key in keys:
+        params[key] = ""
     for key in keys:
         if not key in request.args:
-            return render_template("index.html")
+            return render_template("index.html", params=params, data=[])
         params[key] = request.args.get(key)
 
     search_results = getSearchResults(params)
-    return render_template("index.html", params=params)
+    return render_template("index.html", params=params, data=search_results)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- functions.py
+++ functions.py
@@ -1,13 +1,31 @@
 import os,sys
 import json
 from elasticsearch import Elasticsearch
+import requests
 es = Elasticsearch()
 index_name = "atcoder_submissions"
+
+def getContestDict():
+    req = requests.get("https://kenkoooo.com/atcoder/resources/contests.json")
+    req = json.loads(req.text)
+    data = {}
+    for r in req:
+        data[r["id"]] = r["title"]
+    return data
+
+def getProblemDict():
+    req = requests.get("https://kenkoooo.com/atcoder/resources/problems.json")
+    req = json.loads(req.text)
+    data = {}
+    for r in req:
+        data[r["id"]] = r["title"]
+    return data
 
 def getSearchResults(params):
     keyword = params["keyword"]
     user_id = params["user_id"]
     language = params["language"]
+    result = params["result"]
 
     query = {
         "_source": "*",
@@ -30,12 +48,28 @@ def getSearchResults(params):
                                 "user_id"
                             ]
                         }
-                    }
+                    },
+                    {
+                        "query_string": {
+                            "query": result,
+                            "fields": [
+                                "result"
+                            ]
+                        }
+                    },  
                 ]
             }
         }
     }
     res = es.search(index=index_name, body=query)
-    print(res)
+
+    contest_dict = getContestDict()
+    problem_dict = getProblemDict()
+    # print(problem_dict)
+    res = list(map(lambda hit: hit['_source'], res['hits']['hits']))
+    for i in range(len(res)):
+        res[i]['contest_name'] = contest_dict.get(res[i]['contest_id'], '')
+        res[i]['problem_name'] = problem_dict.get(res[i]['problem_id'], '')
+
     return res
 
